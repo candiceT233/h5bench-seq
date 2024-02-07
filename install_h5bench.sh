d19dbb7bd5f27bc590d68bdee7292da9a9dcd0bd
candiceT233 <candicet233@gmail.com>
2024-02-07 13:07:25 -0600

    clean up install script paths

--- install_h5bench.sh
+++ install_h5bench.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+INSTALL_PREFIX=$HOME/install/h5bench
+
 HDF5_PATH="`which h5cc |sed 's/.\{9\}$//'`"
 
 echo "HDF5_HOME: $HDF5_PATH"
 
-INSTALL_DIR=$HOME/install/h5bench
 
 mkdir build
 cd build
 
-export LD_LIBRARY_PATH=/people/tang584/install/hdf5/lib/libhdf5.so.310
+export LD_LIBRARY_PATH=$HDF5_PATH/lib/libhdf5.so.310
 
 cmake .. \
-    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
     -DCMAKE_C_FLAGS="-I/$HDF5_PATH/include \
     -DH5BENCH_ALL=ON \
     -L$HDF5_PATH/lib"
 make 
 make install
+
+# Add path
+echo "export PATH=$INSTALL_PREFIX/bin:$PATH" >> $HOME/.bashrc
 
 # mkdir build
 # cd build
